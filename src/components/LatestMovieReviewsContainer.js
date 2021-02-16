8040561b6f69815ec2b579a043bf6e632ff8926e
pete3249 <katelynrpeterson16@gmail.com>
2021-02-15 23:32:25 -0500

    commit tree 17ee13e8dad40249f1d60e04e15504fb199a1348

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,25 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(response => response.json())
+        .then(reviews => this.setState({
+            reviews: reviews.results
+        }))
+    }
+
+    render(){
+        return (
+            <div className="latest-movie-reviews">
+                <h2>The Latest Movie Reviews</h2>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,42 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends React.Component {
+    state= {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        fetch(URL.concat(this.state.searchTerm))
+        .then(response => response.json())
+        .then(reviews => this.setState({ reviews: reviews.results}))
+    }
+
+    handleChange = event => {
+        this.setState({ searchTerm: event.target.value})
+    } 
+    
+    render() {
+        return (
+            <div>
+                <h2>Search for Latest Reviews</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="search for reviews..."
+                        value={this.state.searchTerm}
+                        onChange={this.handleChange}
+                    />
+                <input type="submit"/>
+                </form>
+                {typeof this.state.reviews === 'object' &&
+                    this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
